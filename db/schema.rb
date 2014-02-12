aab4fe9d57f6c97ecdac12a62e6f8d621a25b303
rails <rails@rails.(none)>
2014-02-12 16:24:27 +0530

    Updated invoice columns

--- db/schema.rb
+++ db/schema.rb
@@ -63,9 +63,9 @@ ActiveRecord::Schema.define(:version => 20140211060412) do
     t.string   "case_name"
     t.integer  "counselor_id"
     t.integer  "intake_form_id"
-    t.string   "status"
-    t.datetime "created_at",     :null => false
-    t.datetime "updated_at",     :null => false
+    t.string   "status",         :default => "inactive"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
   end
 
   create_table "children", :force => true do |t|
