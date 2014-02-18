2f65ed6fb2344da260dba0d36d38496660d48cce
neeraji2it <neeraji2it@gmail.com>
2014-02-18 18:37:41 +0530

    small issue fixed

--- app/controllers/case_managements_controller.rb
+++ app/controllers/case_managements_controller.rb
@@ -7,8 +7,8 @@ class CaseManagementsController < ApplicationController
     @document = Document.new
     session[:ap_id] = params[:appointment_id] if params[:appointment_id]
     @appointment = Appointment.find(session[:ap_id]) if session[:ap_id]
-    @acccount = PayerAccount.where("case_id = #{@case.id} and client_id = #{@appointment.client.id}").last
-    @authcounts = SessionPayment.where("case_id = #{@case.id} and client_id = #{@appointment.client.id}").count
+    @acccount = @appointment.nil? ? PayerAccount.where("case_id = #{@case.id}").last : PayerAccount.where("case_id = #{@case.id} and client_id = #{@appointment.client.id}").last
+    @authcounts = @appointment.nil? ? SessionPayment.where("case_id = #{@case.id}").count : SessionPayment.where("case_id = #{@case.id} and client_id = #{@appointment.client.id}").count
   end
   
   def upload_document
