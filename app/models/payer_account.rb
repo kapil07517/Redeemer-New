14c5e4af60521e68fd37a23863ab946002509095
neeraji2it <neeraji2it@gmail.com>
2014-03-08 14:32:40 +0530

    added start date to fee

--- app/models/payer_account.rb
+++ app/models/payer_account.rb
@@ -1,6 +1,6 @@
 class PayerAccount < ActiveRecord::Base
-  attr_accessible :case_id,:client_id,:payer_id,:auth,:amount
-  validates :case_id,:client_id,:payer_id,:auth,:amount,:presence => true
+  attr_accessible :case_id,:client_id,:payer_id,:auth,:amount,:start_date
+  validates :case_id,:client_id,:payer_id,:auth,:amount,:start_date,:presence => true
   belongs_to :client
   belongs_to :case
   belongs_to :payer
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140304051800) do
+ActiveRecord::Schema.define(:version => 20140308084932) do
 
   create_table "adolesment_intakes", :force => true do |t|
     t.integer  "client_id"
@@ -347,6 +347,7 @@ ActiveRecord::Schema.define(:version => 20140304051800) do
     t.string   "amount"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.date     "start_date"
   end
 
   create_table "payers", :force => true do |t|
