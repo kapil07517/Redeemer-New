e9809159041ddf1ad9e5f1bb1f184694a53e2b82
neeraji2it <neeraji2it@gmail.com>
2014-04-08 17:10:51 +0530

    changed intake details page

--- app/controllers/client/intakes_controller.rb
+++ app/controllers/client/intakes_controller.rb
@@ -7,5 +7,12 @@ class Client::IntakesController < ApplicationController
   
   def show
     @intake_form = IntakeForm.all_information(params[:id]).first
+    if !@intake_form.medical_id.nil?
+      @medical_reasons = MedicalReason.where("medical_id = '#{@intake_form.medical_id}'")
+      @medications = Medication.where("medical_id = '#{@intake_form.medical_id}'")
+    else
+      @medical_reasons = []
+      @medications = []
+    end
   end
 end
